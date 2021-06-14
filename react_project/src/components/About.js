ad1148d81d52909b60f41290b29a13e430a78333
Gregory Kozlov <mr.gregkozlov@gmail.com>
2021-06-14 16:28:58 +0300

    app launch

---
+++ react_project/src/components/About.js
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div className="text">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet perferendis, culpa, vel harum praesentium totam inventore, possimus distinctio rem nulla nisi excepturi quis illum itaque! Et voluptate beatae exercitationem magnam.
+        </div>
+    );
+}
+
+export default About;
---
+++ react_project/src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <>
+            <footer className="page-footer font-small navbar-dark bg-dark ">
+                <div className="container navbar-nav">
+                    <div className="footer-copyright text-center py-3">
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
